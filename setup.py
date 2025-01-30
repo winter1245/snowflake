c72ed01ec5b21640967c370d4d96e211831c8589
“chris <christianwagner03@gmail.com>
2025-01-30 20:59:48 +0100

    move to toml config

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(
-        name='snowflake',
-        version='1.0',
-        install_requires=[''],
-        entry_points={"console_scripts": ["snowflake=snowflake.main:main"]},
-        url="https://github.com/winter1245/snowflake",
-      )
