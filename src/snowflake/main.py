a089a2cf0b461da8b816be2d13e6464447fc6ab3
“chris <christianwagner03@gmail.com>
2025-02-01 23:03:25 +0100

    add quit

--- src/snowflake/main.py
+++ src/snowflake/main.py
@@ -5,20 +5,21 @@ from snowflake.params import args
 def init():
      
    
+    if not args.quiet:
 
-    print("""
+        print("""
   ____                      __ _       _        
 / ___| _ __   _____      __/ _| | __ _| | _____ 
 \\___ \\| '_ \\ / _ \\ \\ /\\ / / |_| |/ _` | |/ / _ \\
  ___) | | | | (_) \\ V  V /|  _| | (_| |   <  __/
 |____/|_| |_|\\___/ \\_/\\_/ |_| |_|\\__,_|_|\\_\\___|                                                                                                                                                                                                                                                           
-    """)
+        """)
     
-    if not os.path.isfile("scope.txt"):
-        print(f"{'\033[93m'}[WARNING]{'\033[0m'} scope.txt not found")
+        if not os.path.isfile("scope.txt"):
+            print(f"{'\033[93m'}[WARNING]{'\033[0m'} scope.txt not found")
     
-    if not os.path.isfile("secret.txt"):
-        print(f"{'\033[93m'}[WARNING]{'\033[0m'} secret.txt not found")
+        if not os.path.isfile("secret.txt"):
+            print(f"{'\033[93m'}[WARNING]{'\033[0m'} secret.txt not found")
 
 
 
