0bb9fda5425886c8b9f7608c7c5b8cf9d9ff35eb
“chris <christianwagner03@gmail.com>
2025-02-01 22:30:30 +0100

    add verbose add scope para

--- src/snowflake/main.py
+++ src/snowflake/main.py
@@ -29,12 +29,21 @@ def controller():
    
     parser = argparse.ArgumentParser(description="seperate scope")
     parser.add_argument("-a", "--all",action="store_true", help="run all scripts successively")
+    parser.add_argument("-sc", "--scope",action="store_true", help="seperate scope")
+    parser.add_argument("-v", "--verbose",action="store_true", help="more verbose logging")
     args = parser.parse_args()
+    
+    if args.scope:
+        formatScope()
+        return
     if args.all:
         formatScope()
+        #...
+        return
+    
 
 
-    return 0
+    return 
 
 
 def main():
