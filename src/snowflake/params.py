1a15807addd9c44c18b487ba5e262cc0fbcb3f0f
“chris <christianwagner03@gmail.com>
2025-02-06 21:33:22 +0100

    add sub param

--- src/snowflake/params.py
+++ src/snowflake/params.py
@@ -4,4 +4,5 @@ parser.add_argument("-a", "--all",action="store_true", help="run all scripts suc
 parser.add_argument("-sc", "--scope",action="store_true", help="seperate scope")
 parser.add_argument("-v", "--verbose",action="store_true", help="more verbose logging")
 parser.add_argument("-q", "--quiet",action="store_true", help="no output to stdout")
+parser.add_argument("-sub", "--subdomains",action="store_true", help="enumerate subdomains")
 args = parser.parse_args()
