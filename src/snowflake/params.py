73079ecc99c7fae4314b69d9f15cfb8584a970bf
“chris <christianwagner03@gmail.com>
2025-02-07 23:08:38 +0100

    fixing random stuff

--- src/snowflake/params.py
+++ src/snowflake/params.py
@@ -1,5 +1,5 @@
 import argparse
-parser = argparse.ArgumentParser(description="seperate scope")
+parser = argparse.ArgumentParser(description="work in progress recon tool")
 parser.add_argument("-a", "--all",action="store_true", help="run all scripts successively")
 parser.add_argument("-sc", "--scope",action="store_true", help="seperate scope")
 parser.add_argument("-v", "--verbose",action="store_true", help="more verbose logging")
