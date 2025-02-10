f96ce35c7785757563096c237effffa218a15bef
“chris <christianwagner03@gmail.com>
2025-02-10 15:29:23 +0100

    rewrite formatter

--- src/snowflake/formatter.py
+++ src/snowflake/formatter.py
@@ -1,47 +1,39 @@
 import sys
 try:
     from snowflake.params import args
+    import snowflake.helper
 except ImportError:
     from params import args
+    import helper
 
 def formatScope():
     
-    with open("scope.txt", "rb") as f:
-        lineCount = sum(1 for _ in f)
+    scope=helper.fromFile('scope.txt')
+    lineCount= len(scope)
+    domains = []
+    alive = []
+    i=0
+    for line in scope:
+        i+=1
+        if not args.quiet:
+            sys.stdout.write('\r')
+            sys.stdout.write(f'{helper.GREEN}[INFO]{helper.WHITE} seperating scope [{str(i)} of {str(lineCount)}]')     
+            sys.stdout.flush()
 
-    with open('scope.txt') as scope:
-        i=0
-        for line in scope:
-            i+=1
-            if not args.quiet:
-                sys.stdout.write('\r')
-                sys.stdout.write(f'{'\033[92m'}[INFO]{'\033[0m'} seperating scope [{str(i)} of {str(lineCount)}]')
-                sys.stdout.flush()
+        if line[0]=='*':
+            domains.append(line)
+            if args.verbose and not args.quiet:
+                print(f"\ncopy {line[:-1]} to wildcard.txt")
 
-            if line[0]=='*':
-               
-                wildcard=line[1:] # remove *
-                if wildcard[0]=='.':
-                    wildcard=wildcard[1:] # remove .
-                try:
-                    with open('wildcard.txt', 'a') as file:
-                        file.write(wildcard)
-                    if args.verbose and not args.quiet:
-                        print(f"\ncopy {line[:-1]} to wildcard.txt")
-
-                except OSError:
-                    print("Writing to wildcard.txt failed")
-
-            else:
-                try:
-                    with open('active.txt', 'a') as file:
-                        file.write("https://" + line)
-                    if args.verbose and not args.quiet:
-                        print(f"\ncopy {line[:-1]} to active.txt")
-
-                except OSError:
-                    print("Writing to active.txt failed")
+        else :
+            alive.append(line)
+            if args.verbose and not args.quiet:
+                print(f"\ncopy {line[:-1]} to alive.txt")
 
 
-        sys.stdout.write('\n')
-        return 
+    helper.writeFile('domains.txt',domains)
+    helper.writeFile('alive.txt', alive)                   
+    helper.removeWildcard('domains.txt')
+    
+    sys.stdout.write('\n')
+    return 
