f4831386de34b04f829fe42b6876ae393357fd6e
“chris <christianwagner03@gmail.com>
2025-02-11 22:36:37 +0100

    add pretty write to file and pretty output

--- src/snowflake/files.py
+++ src/snowflake/files.py
@@ -1,4 +1,4 @@
-from time import time 
+from time import time,sleep 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
@@ -31,10 +31,14 @@ def screenshot():
 
             source = driver.page_source
             soup = BeautifulSoup(source, 'html.parser')
-            print(soup.prettify())
-            sourcelist = soup.prettify().split('\n')
+            
+            try:
+                with open(f'{path}httpsource.html', 'w') as file:
+                    file.write(soup.prettify())
 
-            helper.writeFile(f'{path}httpsource.html',sourcelist)
+            except OSError:
+                print(f"Writing to {path} failed")
+            
             driver.save_screenshot(f'{path}httpscreenshot.png')
 
         except selenium.common.exceptions.WebDriverException:
@@ -44,17 +48,26 @@ def screenshot():
             driver.get(https)
 
             source = driver.page_source
-            sourcelist = source.split('\n')
+            soup = BeautifulSoup(source, 'html.parser')
+            try:
+                with open(f'{path}source.html', 'w') as file:
+                    file.write(soup.prettify())
 
-            helper.writeFile(f'{path}source.html',sourcelist)
+            except OSError:
+                print(f"Writing to {path} failed")
             driver.save_screenshot(f'{path}screenshot.png')
 
         except selenium.common.exceptions.WebDriverException:
             print(https + 'not found')    
-
-
-
+        
+        sleep(2)
+        if not args.quiet:
+            sys.stdout.write('\r')
+            sys.stdout.write(f'{helper.GREEN}[INFO]{helper.WHITE} screenshot website [{fl.index(subdomain)+1} of {len(fl)}]')     
+            sys.stdout.flush()
+    
     driver.quit()
+    print()
         
     return
 
