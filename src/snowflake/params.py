c911a111a58ffed4712f15e17530da0a2cdb0001
“chris <christianwagner03@gmail.com>
2025-02-11 19:41:06 +0100

    try selenium

--- src/snowflake/params.py
+++ src/snowflake/params.py
@@ -6,5 +6,6 @@ parser.add_argument("-v", "--verbose",action="store_true", help="more verbose lo
 parser.add_argument("-q", "--quiet",action="store_true", help="no output to stdout")
 parser.add_argument("-sub", "--subdomains",action="store_true", help="enumerate subdomains")
 parser.add_argument("-th", "--threading",action="store_true", help="enable multithreading")
+parser.add_argument("-c", "--cycle",action="store_true", help="do a cycle")
 parser.add_argument("-brute", "--bruteforce",action="store_true", help="bruteforce subdommains and resolve through dns queries")
 args = parser.parse_args()
