2f44c51783be89d0deaa45282a4801e2a9eef294
“chris <christianwagner03@gmail.com>
2025-02-12 22:18:32 +0100

    imv wrapper

---
+++ src/snowflake/wrapper.py
@@ -0,0 +1,26 @@
+import subprocess
+
+try:
+    from snowflake.params import args
+    import snowflake.helper as helper
+except ImportError:
+    from params import args
+    import helper
+
+
+
+
+def imv():
+    
+    p=subprocess.run('imv data/*/*/screenshot.png', shell=True,stdout=subprocess.PIPE)
+    out=p.stdout.decode('utf8').split('\n')
+    out=out[:-1]
+    list=[]
+    for i in range(len(out)):
+        out[i]=out[i].split('/')[-3]
+        out[i]=out[i].replace('_','.')
+        list.append('https://' + out[i] + '\n')
+        print('https://' + out[i])
+
+    helper.writeFile('current.txt',list)
+    return
