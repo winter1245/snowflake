b49586bd5fd70b0288fca552f6d82c415b2834cd
“chris <christianwagner03@gmail.com>
2025-02-13 15:05:23 +0100

    fix output

--- src/snowflake/files.py
+++ src/snowflake/files.py
@@ -24,7 +24,7 @@ def probe():
         try:
             r = requests.get(http)
             alive.append(http + '\n')
-            helper.write('data/statuscodes.txt',f'{r.status_code} {http}')
+            helper.append('data/statuscodes.txt',f'{r.status_code} {http}\n')
 
         except requests.exceptions.HTTPError:
             print ("Http Error")
@@ -38,7 +38,7 @@ def probe():
         try:
             r = requests.get(https)
             alive.append(https + '\n')
-            helper.append('data/statuscodes.txt',f'{r.status_code} {https}')
+            helper.append('data/statuscodes.txt',f'{r.status_code} {https}\n')
     
 
         except requests.exceptions.RequestException:  
